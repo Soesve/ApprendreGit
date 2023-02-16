cbc36963cc2a77c06828270f389c4027793db355
Soesve <fourche.quentin@gmail.com>
2023-02-16 14:58:00 +0100

    ajout de test.py

---
+++ test.py
@@ -0,0 +1,55 @@
+
+# GitHub
+# 1/ Sécurité sur la main 
+# 2/ Créer la branche dev
+# 3/ dev devient par défault + sécurité sur dev
+# 4/ Créer une branche feature à partir de dev
+
+# Local
+# 5/ pull depuis le parent
+# 6/ branch n'affichera que la main, il faut se rendre sur la branche pour la voir
+# 7/ switch features
+# 8/ ajout de fichier
+# 9/ Impossible de switch branch sans avoir commit
+# 10/ add + commit
+# 11/ merge dev
+# 12/ On règle les conflits
+# 13/ add + commit
+# 14/ refaire merge dev pour être sur ! ("Already up to date.")
+# 15/ push origin features
+
+# GitHub
+# compare & pull request (base: dev <- compare:features)
+# create pull request
+# attendre une review d'un collaborateur
+
+# Local
+# switch dev
+# checkout -b "feature2"  (créer + se déplacer dans la nouvelle branche)
+# nouveau fichier + modif, etc
+# add + commit
+# push origin feature2 (ça va créer une branche sur GitHub)
+# Pas obligé de pull request tout de suite sur GitHub
+# modif, etc
+# add + commit
+# push origin feature2
+# Une fois qu'on a terminé cette feature
+# git switch dev
+# git pull
+# switch feature2
+# git merge dev
+
+# GitHub
+# pull request
+
+
+
+
+
+
+
+
+
+
+
+
